915a17fe8ed2b6a6e935810259b507a76442a39b
trijayatangguhcbl-tech <trijayatangguh.cbl@gmail.com>
2026-09-25 10:01:45 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -11,7 +11,7 @@ function login(){
         );
 
         location.href =
-        "dashboard.html";
+        "Dashboard.html";
     }
 
     else if(password === "supervisor"){
@@ -22,7 +22,7 @@ function login(){
         );
 
         location.href =
-        "dashboard.html";
+        "Dashboard.html";
     }
 
     else{
